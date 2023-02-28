63af5d3890e07f17d929f70c7db1ac4772608b61
Peter Quiring <pquiring@gmail.com>
2023-02-27 22:39:34 -0500

    add gpg key to arch repo

--- arch/x86_64/update.sh
+++ arch/x86_64/update.sh
@@ -2,4 +2,10 @@
 for f in *.pkg.tar.xz; do
   gpg --detach-sign --no-armor $f
 done
+
 repo-add --verify --sign javaforce.db.tar.gz *.pkg.tar.xz
+
+if [ ! -f javaforce.gpg ]; then
+  cp ~/.gnupg/pubring.gpg ./javaforce.gpg
+  chmod 644 javaforce.gpg
+fi
