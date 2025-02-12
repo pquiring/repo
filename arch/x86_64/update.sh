fa5121c1bbf758f96bdcb0eb491bce8d6f4acaa0
Peter Quiring <pquiring@gmail.com>
2025-02-12 09:43:16 -0500

    arch : update gpg every time

--- arch/x86_64/update.sh
+++ arch/x86_64/update.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
+
+cp ~/.gnupg/pubring.gpg ./javaforce.gpg
+chmod 644 javaforce.gpg
+
 for f in *.pkg.tar.xz; do
   gpg --detach-sign --no-armor $f
 done
 
 repo-add --verify --sign javaforce.db.tar.gz *.pkg.tar.xz
-
-if [ ! -f javaforce.gpg ]; then
-  cp ~/.gnupg/pubring.gpg ./javaforce.gpg
-  chmod 644 javaforce.gpg
-fi
