baac8ea226ad4c87f20597d31ba56fb1e60a127e
Peter Quiring <pquiring@gmail.com>
2025-06-02 10:01:47 -0400

    clean.sh : remove .gpg files

--- arch/x86_64/clean.sh
+++ arch/x86_64/clean.sh
@@ -3,3 +3,4 @@ rm *.xz 2>/dev/null
 rm *.db 2>/dev/null
 rm *.files 2>/dev/null
 rm *.gz 2>/dev/null
+rm *.gpg 2>/dev/null
--- debian/amd64/clean.sh
+++ debian/amd64/clean.sh
@@ -1,4 +1,5 @@
 rm *.deb 2>/dev/null
+rm *.gpg 2>/dev/null
 rm Release 2>/dev/null
 rm Release.gpg 2>/dev/null
 rm Packages 2>/dev/null
