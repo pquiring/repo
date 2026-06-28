c62617bb612efee947441e6c7cd9694c68cfd3ff
Peter Quiring <pquiring@gmail.com>
2026-06-27 22:10:54 -0400

    add centos 10 repo

---
+++ centos/bin/update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+function update() {
+
+  if [ "$1" == "" ]; then
+    echo usage : update.sh {system}
+    return
+  fi
+
+  if [ ! -f ../../bin/$1.repo ]; then
+    echo That system was not found!
+    return
+  fi
+
+  cp ../../bin/$1.repo .
+  cp ../../bin/rpmmacros ~/.rpmmacros
+  gpg --export -a > RPM-GPG-KEY-$1
+  rpm --resign *.rpm
+  createrepo .
+
+}
+
+update $1
